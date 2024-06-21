8c5fa09fa83ae06c7e7116cc3caee9fbd662ea76
Wellington Oliveira <wellingtonoliwagner@gmail.com>
2024-06-21 16:16:23 +0000

    Adicionado script verificacao-memoria-consumida.sh

---
+++ scripts/verificacao-memoria-consumida.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+memoria_total=$(free | grep -i mem | awk -F" " '{ print $2 }')
+memoria_consumida=$(free | grep -i mem | awk -F" " '{ print $3 }')
+relacao_memoria_atual_total=$(bc <<< "scale=2;$memoria_consumida/$memoria_total * 100" | awk -F. '{ print $1 }')
+
+
+
+if [ $relacao_memoria_atual_total -gt 50 ]
+then
+mail -s "Consumo de memoria acima do limite" coteste2024@gmail.com<<del
+O consumo de memoria esta acima do limite que foi especificado. Atualmente o consumo e de $(free -h | grep -i mem | awk '{ print $3 }')
+del
+fi
